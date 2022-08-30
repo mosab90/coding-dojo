67211a1e7e5a9b1fd767acc2564f8d31aefcad1d
mosab93 <mosabmohammad86@gmail.com>
2022-08-30 23:45:39 +0300

    hello

---
+++ Web Fundamentals/javascript/pizza/pizza.js
@@ -0,0 +1,17 @@
+function pizza(bcrustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.bcrustType = bcrustType;
+    pizza.sauceType= sauceType;
+    pizza.cheeses= cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var s1 = pizza("flower", "bbq", "3mek", ["olive", "tommeto slice", "mashrom"]);
+var s2 = pizza("fdeep dish", "btraditional", ["mozzarella"], ["pepperoni", "sausage"]);
+var s3 = pizza("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+var s4 = pizza("fdeep dish", "btraditional", "mozzarella", ["mushrooms", "onions"]);
+var s5 = pizza("fdeep dish", "btraditional", "mozzarella", ["mushrooms", "olives"]);
+
+console.log(s3);
+
