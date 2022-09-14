28c8893e12ced0eb5e884efc485fee509f73494d
mosab93 <mosabmohammad86@gmail.com>
2022-09-14 14:36:48 +0300

    Basic I

---
+++ Python/_python/python fundamentals/Basic I/Basic I.py
@@ -0,0 +1,40 @@
+# for x in range(0,151,1):
+#     print(x)
+
+
+# for x in range(0,1000,5):
+#     print(x)
+
+# def dojo():
+#     for i in range (1,101,1):
+#         if i % 5 == 0:
+#             print ('Coding')
+#         if i % 10 == 0:
+#             print ('Dojo')
+# dojo()            
+
+
+# minimum = 0
+# maximum = 500000
+# total = 0
+
+# for number in range(minimum, maximum+1):
+#     if(number % 2 != 0):
+#         print("{0}".format(number))
+#         Oddtotal = total + number
+
+# def countdown():
+#     number = 2018
+#     while number > 0:
+#         print (number)
+#         number = number - 4
+        
+# countdown()   
+
+
+# def flex(low, high, mult):
+#     for i in range (low, high):
+#         if i % mult == 0:
+#             print (i)
+            
+# flex(2, 9, 3)
